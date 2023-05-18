fc771e5a1e22f1d288d077373dac31f736a2e758
Deedikjepijn <joris8393@gmail.com>
2023-05-18 20:07:30 +0200

    updated main.cpp making a startup menu and a cli menu

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <string>
 #include <stdio.h>
+#include <sstream>
 #include <Windows.h>
 
 using namespace std;
+
+
+int FirstSetup() {
+    cout << "Welcome to the First Setup!" << endl;
+    cout << "Please select a startup option: " << endl;
+    int StartupSelection = 0;
+    cin >> StartupSelection;
+}
 
 int startup() {
     for (int i = 1; i <= 2; ++i) {
@@ -14,7 +23,61 @@ int startup() {
     }
     Sleep(500);
     cout << endl << "Program Initialized!" << endl << endl;
+    return mainMenu();
+}
+
+int silentStartup() {
+    cout << "Initializing Silent startup..." << endl;
+    Sleep(500);
+    cout << "Program Initialized!" << endl;
+    return mainMenu();
+}
+
+int debugStartup() {
+    cout << "Initializing Debug startup..." << endl;
+    Sleep(500);
+    cout << "Program Initialized!" << endl;
+    return mainMenu();
+}
+
+int optionsMenu() {
+    cout << "Welcome to the Options Menu!" << endl;
+    cout << "Please select an option:" << endl;
+    cout << "1. Change startup options" << endl;
+    cout << "2. Change Health Steps" << endl;
+    cout << "3. Change Ammo Steps" << endl;
+    int SettingsSelection = 0;
+    cin >> SettingsSelection;
+    switch (SettingsSelection) {
+        case 1:
+            cout << "Changing Startup Options..." << endl;
+            Sleep(500);
+            cout << "Startup Options Changed!" << endl;
+            break;
+        case 2:
+            cout << "Changing Health Steps..." << endl;
+            Sleep(500);
+            cout << "Health Steps Changed!" << endl;
+            break;
+        case 3:
+            cout << "Changing Ammo Steps..." << endl;
+            Sleep(500);
+            cout << "Ammo Steps Changed!" << endl;
+            break;
+        default:
+            cout << "Invalid Selection!" << endl;
+            Sleep(2000);
+            break;
+    }
     return 0;
+}
+
+int healthSteps() {
+    cout << "Please enter the new health steps: ";
+    int healthSteps = 0;
+    cin >> healthSteps;
+    cout << "Health steps changed to " << healthSteps << "!" << endl;
+    return optionsMenu();
 }
 
 int mainMenu() {
@@ -36,7 +99,7 @@ int mainMenu() {
             cout << "Opening Options..." << endl;
             Sleep(500);
             cout << "Options Opened!" << endl;
-            break;
+            optionsMenu();
         case 3:
             cout << "Exiting Program..." << endl;
             Sleep(500);
@@ -45,13 +108,30 @@ int mainMenu() {
         default:
             cout << "Invalid Selection!" << endl;
             Sleep(2000);
-            return mainMenu(); //bugged fuck this
+            break;
     }
     return 0;
 }
 
 int main() {
-    startup();
-    mainMenu();
+    if (true) { // if config file doesnt exsist then run first setup
+        FirstSetup();
+    }
+    else if (true) { // if config file states normal startup
+        startup();
+    }
+    else if (true) { // if config file states silent startup
+        silentStartup();
+    }
+    else if (true) { // if config file states debug startup
+        debugStartup();
+    }
+    else {
+        cout << "Error: Config file is corrupted!" << endl;
+        Sleep(2000);
+        cout << "Please run First Setup!" << endl;
+        Sleep(2000);
+        FirstSetup();
+    }
     return 0;
 }
